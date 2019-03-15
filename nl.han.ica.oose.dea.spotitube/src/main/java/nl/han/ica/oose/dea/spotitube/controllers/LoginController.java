65da13e7d79853b2670266b4627c8fa69ea689cd
Sven Molhuijsen <s.molhuijsen@gmail.com>
2019-03-15 12:08:11 +0100

    project set up

---
+++ nl.han.ica.oose.dea.spotitube/src/main/java/nl/han/ica/oose/dea/spotitube/controllers/LoginController.java
@@ -0,0 +1,5 @@
+package nl.han.ica.oose.dea.spotitube.controllers;
+
+public class LoginController {
+    public UserModel login(String username, Strin )
+}
---
+++ nl.han.ica.oose.dea.spotitube/src/main/java/nl/han/ica/oose/dea/spotitube/models/UserModel.java
@@ -0,0 +1,43 @@
+package nl.han.ica.oose.dea.spotitube.models;
+
+public class UserModel {
+
+
+    String fullName;
+    String token;
+    String email;
+    String tel;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+}
